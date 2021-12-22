886af6b511e2421d22bbf4f0e7d02df1779e3b85
Marco Braga <92943261+mBraga28@users.noreply.github.com>
2021-12-22 16:19:07 -0300

    Delete workspace_HTML5_CSS_JS directory

--- workspace_HTML5_CSS_JS/js/ex1.js
+++
@@ -1,9 +0,0 @@
-// var numero = 10;
-// console.log(numero);
-
-// let segundoNumero = 20;
-// console.log(segundoNumero);
-
-let terceiroNumero = 30;
-console.log(terceiroNumero);
-terceiroNumero = 10;
